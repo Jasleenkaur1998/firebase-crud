59cd4da18f0d40661ec36398279f77a1d632d7ef
Jasleen Kaur <jasleen2357@gmail.com>
2023-10-25 13:46:53 -0700

    Adding a user info to firebase database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { db } from './firebase-config';
 import './App.css';
 
 function App() {
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState('');
   const [users, setUsers] = useState([]);
   const usersRef = collection(db, 'users');
 
@@ -15,15 +17,25 @@ function App() {
     getUsers();
   }, []);
 
+  const addUser = async (e) => {
+    await addDoc(usersRef, { name: newName, age: newAge });
+  }
+
   return (
     <div className="App">
      <h1>User Details</h1>
+      <input className="input-field" placeholder='Name..' onChange={(event) => setNewName(event.target.value)} />
+      <input className="input-field" placeholder='Age..' onChange={(event) => setNewAge(event.target.value)}/>
+      <button type="button" className="add-button" onClick={addUser}>Add User</button>
+      <div className="user-container">
         {users.map((user) => {
-          return <div>
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-          </div>
+          return (
+            <div className="user" key={user.id}>
+              <div><u>Name:</u> {user.name} <br /><u>Age:</u> {user.age}</div>
+            </div>
+          );
         })}
+      </div>
     </div>
   );
 }
