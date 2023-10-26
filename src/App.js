d49e595d80a4bd14632aebcbac70dc2cb61d2768
Jasleen Kaur <jasleen2357@gmail.com>
2023-10-26 00:15:43 -0700

    Added the update age feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, doc, getDocs, updateDoc } from 'firebase/firestore';
 import { db } from './firebase-config';
 import './App.css';
 
@@ -21,17 +21,30 @@ function App() {
     await addDoc(usersRef, { name: newName, age: +newAge });
   }
 
+  const handleAgeChange = async (id, age) => {
+    const userDoc = doc(db, 'users', id);
+    const newField = { age: age + 1 }
+    await updateDoc(userDoc, newField);
+  }
+
   return (
     <div className="App">
      <h1>User Details</h1>
       <input className="input-field" placeholder='Name..' onChange={(event) => setNewName(event.target.value)} />
-      <input className="input-field" placeholder='Age..' onChange={(event) => setNewAge(event.target.value)}/>
+      <input className="input-field" placeholder='Age..' onChange={(event) => setNewAge(event.target.value)} />
       <button type="button" className="add-button" onClick={addUser}>Add User</button>
       <div className="user-container">
         {users.map((user) => {
           return (
             <div className="user" key={user.id}>
-              <div><u>Name:</u> {user.name} <br /><u>Age:</u> {user.age}</div>
+              <div>
+                <u>Name:</u> {user.name} <br />
+                <u>Age:</u> {user.age}
+                <button
+                  onClick={() => {
+                    handleAgeChange(user.id, user.age);
+                  }} className='age-button'>Edit Age</button>
+              </div>
             </div>
           );
         })}
